c05d455145d697f8e332753acaf4ba9d948ef079
Alice <coolalisa@yandex.ru>
2020-05-30 23:37:21 +0300

    Implemented NewsDelegateAdapter.

---
+++ app/src/main/java/com/alisadmitrieva/keddit/adapter/NewsDelegateAdapter.kt
@@ -0,0 +1,33 @@
+package com.alisadmitrieva.keddit.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.alisadmitrieva.keddit.R
+import com.alisadmitrieva.keddit.commons.RedditNewsItem
+import com.alisadmitrieva.keddit.commons.getFriendlyTime
+import com.alisadmitrieva.keddit.commons.inflate
+import kotlinx.android.synthetic.main.news_item.view.*
+
+class NewsDelegateAdapter : ViewTypeDelegateAdapter {
+
+    override fun onCreateViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        return TurnsViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, item: ViewType) {
+        holder as TurnsViewHolder
+        holder.bind(item as RedditNewsItem)
+    }
+
+    class TurnsViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        parent.inflate(R.layout.news_item)) {
+
+        fun bind(item: RedditNewsItem) = with(itemView) {
+            img_thumbnail.loadImg(item.thumbnail)
+            description.text = item.title
+            author.text = item.author
+            comments.text = "${item.numComments} comments"
+            time.text = item.created.getFriendlyTime()
+        }
+    }
+}
