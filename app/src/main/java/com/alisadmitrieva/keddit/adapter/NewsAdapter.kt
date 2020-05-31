e4023c208205c57e100e29b889ebfb76779462ab
Alice <coolalisa@yandex.ru>
2020-05-31 23:47:24 +0300

    Changed NewsAdapter.

--- app/src/main/java/com/alisadmitrieva/keddit/adapter/NewsAdapter.kt
+++ app/src/main/java/com/alisadmitrieva/keddit/adapter/NewsAdapter.kt
@@ -3,6 +3,7 @@ package com.alisadmitrieva.keddit.adapter
 import android.view.ViewGroup
 import androidx.collection.SparseArrayCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.alisadmitrieva.keddit.commons.RedditNewsItem
 import java.util.*
 
 class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -29,10 +30,40 @@ class NewsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        delegateAdapters.get(getItemViewType(position))?.onBindViewHolder(holder, this.items[position])
+        delegateAdapters.get(getItemViewType(position))
+            ?.onBindViewHolder(holder, this.items[position])
     }
 
     override fun getItemViewType(position: Int): Int {
         return this.items.get(position).getViewType()
     }
+
+    fun addNews(news: List<RedditNewsItem>) {
+        // first remove loading and notify
+        val initPosition = items.lastIndex
+        items.removeAt(initPosition)
+        notifyItemRemoved(initPosition)
+
+        // insert news and the loading at the end of the list
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeChanged(initPosition, items.size + 1 /* plus loading item */)
+    }
+
+    fun clearAndAddNews(news: List<RedditNewsItem>) {
+        items.clear()
+        notifyItemRangeRemoved(0, getLastPosition())
+
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeInserted(0, items.size)
+    }
+
+    fun getNews(): List<RedditNewsItem> {
+        return items
+            .filter { it.getViewType() == AdapterConstants.NEWS }
+            .map { it as RedditNewsItem }
+    }
+
+    private fun getLastPosition() = if (items.lastIndex == -1) 0 else items.lastIndex
 }
