58f14bb5ffde293cd9163c3aa63712f0b1b63b13
Alice <coolalisa@yandex.ru>
2020-06-06 23:52:43 +0300

    Fixed not-null issues in NewsFragment.

--- app/src/main/java/com/alisadmitrieva/keddit/NewsFragment.kt
+++ app/src/main/java/com/alisadmitrieva/keddit/NewsFragment.kt
@@ -15,6 +15,10 @@ import rx.schedulers.Schedulers
 
 class NewsFragment : RxBaseFragment() {
 
+    companion object {
+        private val KEY_REDDIT_NEWS = "redditNews"
+    }
+
     private var redditNews: RedditNews? = null
     private val newsList by lazy {
         news_list
@@ -33,31 +37,32 @@ class NewsFragment : RxBaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        news_list.setHasFixedSize(true)
-        val linearLayout = LinearLayoutManager(context)
-
-        news_list.layoutManager = linearLayout
-        news_list.clearOnScrollListeners()
-        news_list.addOnScrollListener(InfiniteScrollListener({ requestNews() }, linearLayout))
+        news_list.apply {
+            setHasFixedSize(true)
+            val linearLayout = LinearLayoutManager(context)
+            news_list.clearOnScrollListeners()
+            layoutManager = linearLayout
+            clearOnScrollListeners()
+            addOnScrollListener(InfiniteScrollListener({ requestNews() }, linearLayout))
+        }
         initAdapter()
 
-        if (savedInstanceState == null) {
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(
-                    RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "https://picsum.photos/200/200?image=$i",
-                        "url"
-                    )
-                )
-            }
+        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_REDDIT_NEWS)) {
+            redditNews = savedInstanceState.get(KEY_REDDIT_NEWS) as RedditNews
+            (news_list.adapter as NewsAdapter).clearAndAddNews(redditNews!!.news)
+        } else {
+            requestNews()
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val news = (news_list.adapter as NewsAdapter).getNews()
+        if (redditNews != null && news.size > 0) {
+            outState.putParcelable(KEY_REDDIT_NEWS, redditNews?.copy(news = news))
+        }
+    }
+    
     fun requestNews() {
         val subscription = newsManager.getNews(redditNews?.after ?: "")
             .subscribeOn(Schedulers.io())
